28d8ca7f25e865fc8d581e4c5a9fb6872dbd42e6
InvManSuite <invmansuite@gmail.com>
2019-09-15 13:17:40 -0400

    Add HttpClientModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
-import { ReactiveFormsModule, FormsModule } from "@angular/forms";
-import { AppRoutingModule } from "./app-routing.module";
-import { AppComponent } from "./app.component";
-import { InventoryListComponent } from "./pages/inventory-list/inventory-list.component";
-import { InventoryItemComponent } from "./pages/inventory-item/inventory-item.component";
-import { HeaderComponent } from "./header/header.component";
-import { FooterComponent } from "./footer/footer.component";
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {ReactiveFormsModule, FormsModule} from '@angular/forms';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {InventoryListComponent} from './pages/inventory-list/inventory-list.component';
+import {InventoryItemComponent} from './pages/inventory-item/inventory-item.component';
+import {HeaderComponent} from './header/header.component';
+import {FooterComponent} from './footer/footer.component';
+import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -17,8 +18,9 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
     HeaderComponent,
     FooterComponent
   ],
-  imports: [BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule, FontAwesomeModule],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule, FontAwesomeModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule {
+}
--- src/app/pages/inventory-list/inventory-list.component.ts
+++ src/app/pages/inventory-list/inventory-list.component.ts
@@ -1,37 +1,52 @@
-import { Component, OnInit } from "@angular/core";
-import { FormControl, FormGroup } from "@angular/forms";
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup} from '@angular/forms';
+import {HttpClient} from '@angular/common/http';
 
 export interface Item {
   name: string;
   quantity: number;
-  description?: string;
+  description: string;
 }
 
 @Component({
-  selector: "app-inventory-list",
-  templateUrl: "./inventory-list.component.html",
-  styleUrls: ["./inventory-list.component.css"]
+  selector: 'app-inventory-list',
+  templateUrl: './inventory-list.component.html',
+  styleUrls: ['./inventory-list.component.css']
 })
 export class InventoryListComponent implements OnInit {
   inventoryForm = new FormGroup({
-    name: new FormControl(""),
-    quantity: new FormControl("")
+    name: new FormControl(''),
+    quantity: new FormControl(''),
+    description: new FormControl('')
   });
 
-  constructor() {}
+  private httpClient: HttpClient;
 
-  public itemList: Item[] = [
-    { name: "shirt", quantity: 5, description: '' },
-    { name: "top", quantity: 2, description: '' }
+  constructor(private http: HttpClient) {
+    this.httpClient = http;
+  }
+
+  public items: Item[] = [
+    {name: 'shirt', quantity: 5, description: 'White shirt for men'},
+    {name: 'top', quantity: 2, description: 'Nice sweater for outdoors'}
   ];
+
+  get() {
+    this.httpClient
+      .get('https://localhost:44309/api/items')
+      .subscribe((data) => console.log(data));
+  }
 
   onSubmit() {
     console.log(this.inventoryForm);
 
-    this.itemList.push({
-      name: this.inventoryForm.value['name'],
-      quantity: this.inventoryForm.value['quantity']
+    this.items.push({
+      name: this.inventoryForm.controls.name.value,
+      quantity: this.inventoryForm.controls.quantity.value,
+      description: this.inventoryForm.controls.description.value
     });
   }
-  ngOnInit() {}
+
+  ngOnInit() {
+  }
 }
